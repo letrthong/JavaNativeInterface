6bf6ba80897ca9e7598d67fd65ec6b3cefbbb590
ThongLT <letrthong@gamil.com>
2016-08-17 00:24:14 +0700

    build x64

--- example1/run.sh
+++ example1/run.sh
@@ -1,9 +1,18 @@
 
 javac Hello.java
+
+echo "Generate Header file"
 javah Hello
 
-gcc -o libhello.so -shared -Wl,-soname,libhello.so  -I/usr/lib/jvm/default-java/include/ -I/usr/local/java/include/linux Hello.c -static -lc
+echo "Creat so file"
+#32bit
+#gcc -o libhello.so -shared -Wl,-soname,libhello.so  -I/usr/lib/jvm/default-java/include/ -I/usr/local/java/include/linux Hello.c -static -lc
 
+
+#64 bit
+gcc -o libhello.so -fPIC -shared -Wl,-soname,libhello.so  -I/usr/lib/jvm/default-java/include/ -I/usr/local/java/include/linux Hello.c -Bstatic -lc
+
+echo "Verify result"
 export LD_LIBRARY_PATH=./
 java Hello
 
